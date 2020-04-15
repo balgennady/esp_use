ad58b8979077f2453a5b8519e50bf30bd9d3a7f5
Gennady Bal <balgena2@gmail.com>
2020-04-15 15:19:49 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,12 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
+#include <WiFiClient.h> // клиент, который может соединяться с указанным IP-адресом через определенный порт 
 #include <ESP8266WebServer.h>
+/**
+ * веб-сервер, который знает, как обрабатывать HTTP-запросы, такие как GET и POST, 
+ * и может поддерживать только один одновременный клиент
+ */
 #include <WebSocketsServer.h>
 
 void setup() {
