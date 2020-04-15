060bf81448b9a3044f3482035c5d0218d4b82c29
Gennady Bal <balgena2@gmail.com>
2020-04-15 15:02:03 +0300

    init

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+/**
+ * Устройство с ESP8266, отправка данных в реальном времени
+ * https://esp8266.ru/forum/threads/ustrojstvo-s-esp8266-otpravka-dannyx-v-realnom-vremeni.4874/post-71451
+ */
+
 #include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
 
 void setup() {
   // put your setup code here, to run once:
